fd2303ec0171d814880e889850f65677c2ef5a4b
bill geng <billgeng@yahoo.com>
2021-11-07 17:04:29 -0500

    add server.js

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const {typeDefs, resolvers} = require('./schemas');
 const db = require('./config/connection');
 
 const routes = require('./routes');
+const {authMiddleware} = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;

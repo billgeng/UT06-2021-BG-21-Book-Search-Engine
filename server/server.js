b8a7b3f1944e08f0da5e9bdfbbed877c2330d5da
bill geng <billgeng@yahoo.com>
2021-11-07 22:56:02 -0500

    add index.html in client

--- server/server.js
+++ server/server.js
@@ -12,6 +12,15 @@ const routes = require('./routes');
 const {authMiddleware} = require('./utils/auth');
 
 const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+server.applyMiddleware({app});
+
+
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
@@ -23,7 +32,13 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.use(routes);
+app.get('*',(req,res) =>{
+  res.sendFile(path.join(__dirname,'../client/build/index.html'));
+});
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}!`);
+    console.log(`Use Graphql at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });

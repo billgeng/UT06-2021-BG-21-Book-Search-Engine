359681a7467025ec05d36aac8f587bc10d3ea8e3
bill geng <billgeng@yahoo.com>
2021-11-08 11:13:11 -0500

    change server errors

--- server/server.js
+++ server/server.js
@@ -13,18 +13,23 @@ const {authMiddleware} = require('./utils/auth');
 
 const PORT = process.env.PORT || 3001;
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware
-});
+
 const app = express();
 
-const startup = async () => {
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware
+  });
+
   await server.start();
   server.applyMiddleware({ app });
-  
-}
+
+  console.log(`Use Graphql at http://localhost:${PORT}${server.graphqlPath}`);
+  };
+
+  startServer();
 
 
 app.use(express.urlencoded({ extended: false }));
@@ -43,6 +48,6 @@ app.get('*',(req,res) =>{
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`🌍 Now listening on localhost:${PORT}`);
-    console.log(`Use Graphql at http://localhost:${PORT}${server.graphqlPath}`);
+    
   });
 });
